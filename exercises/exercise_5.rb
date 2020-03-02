0d45677025c41519a16311fdcba0d24791917477
yoon soon kang <kangyoonsoon@hotmail.com>
2020-03-02 23:17:26 +0000

    ex5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,19 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total = Store.sum(:annual_revenue)
+puts @total
+@average = Store.average(:annual_revenue)
+puts @average
+
+
+
+@richStore = Store.where('annual_revenue > ?', 1000000).count
+puts @richStore
+
+
+@_richStore = Store.where('annual_revenue > ?', 1000000)
+for eachOne in @_richStore
+  puts eachOne.name
+end
