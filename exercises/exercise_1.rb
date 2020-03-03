de960dbe5f0fc05939a39c6d008527a623e7b32c
yoon soon kang <kangyoonsoon@hotmail.com>
2020-03-03 01:03:01 +0000

    during ex7, could not finish yet

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -5,35 +5,35 @@ puts "----------"
 
 
 
-s1 = Store.new
-s1.name = "Burnaby"
-s1.annual_revenue = 300000
+@s1 = Store.new
+@s1.name = "Burnaby"
+@s1.annual_revenue = 300000
 # s1.count(s1.annual_revenue)
-s1.mens_apparel = true
-s1.womens_apparel = true
+@s1.mens_apparel = true
+@s1.womens_apparel = true
 # puts s1.count
-s1.save
+@s1.save
 
-s2 = Store.new
-s2.name = "Richmond"
-s2.annual_revenue = 1260000
+@s2 = Store.new
+@s2.name = "Richmond"
+@s2.annual_revenue = 1260000
 # s2.count(s2.annual_revenue)
  
-s2.mens_apparel = false
-s2.womens_apparel = true
+@s2.mens_apparel = false
+@s2.womens_apparel = true
 # puts s2.count
-s2.save
+@s2.save
 
 
-s3 = Store.new
-s3.name = "Gastown"
-s3.annual_revenue = 190000
+@s3 = Store.new
+@s3.name = "Gastown"
+@s3.annual_revenue = 190000
 # s3.count(s3.annual_revenue)
 
-s3.mens_apparel = true
-s3.womens_apparel = false
+@s3.mens_apparel = true
+@s3.womens_apparel = false
 # puts s3.count
-s3.save
+@s3.save
 
 puts Store.count
 
--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,16 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+
+@s1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@s1.employees.create(first_name: "John", last_name: "Millan", hourly_rate: 50)
+@s1.employees.create(first_name: "Michell", last_name: "Kuran", hourly_rate: 40)
+
+@s1.save
+
+
+@s2.employees.create(first_name: "Nimi", last_name: "Yan", hourly_rate: 65)
+@s2.employees.create(first_name: "Nolan", last_name: "Lyon", hourly_rate: 55)
+@s2.employees.create(first_name: "HyunJin", last_name: "Ryu", hourly_rate: 25)
+# @s2.employees.create(first_name: "",last_name: "Yoo", hourly_rate: 250)
+@s2.save
